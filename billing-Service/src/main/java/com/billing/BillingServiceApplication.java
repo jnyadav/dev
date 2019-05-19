1d30f0f4b094c13aeaf6351adecdc964fe1f460a
Manish Yadav <myadav@manh.com>
2019-05-19 14:12:24 +0530

    Appartment Billing Service - initial commit

--- billing-Service/src/main/java/com/billing/BillingServiceApplication.java
+++ billing-Service/src/main/java/com/billing/BillingServiceApplication.java
@@ -1,5 +1,7 @@
 package com.billing;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class BillingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
+		
+		
 	}
 
 }
---
+++ billing-Service/src/main/java/com/billing/entity/MonthlyCharge.java
@@ -0,0 +1,65 @@
+package com.billing.entity;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="monthly_charge")
+public class MonthlyCharge {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	@JsonIgnore
+	private Long id;
+	
+	@Column(name = "per_Unit_Charge")
+	private Short perUnitCharge;
+	
+	@Column(name = "effective_Date")
+	private String effectiveDate;
+	
+	public MonthlyCharge() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Short getPerUnitCharge() {
+		return perUnitCharge;
+	}
+
+	public void setPerUnitCharge(Short perUnitCharge) {
+		this.perUnitCharge = perUnitCharge;
+	}
+
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(String effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	@Override
+	public String toString() {
+		return "MonthlyCharge [perUnitCharge=" + perUnitCharge + ", effectiveDate=" + effectiveDate + "]";
+	}
+	
+	
+
+}
